581ce07428723b0810dd2aeb8e923d2e4223037b
eudoxyz <eudoxyz@localhost.localdomain>
2017-09-20 00:49:58 +0200

    Dodaj: data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+const territories = [
+
+  // North America
+  //
+  /* 01 */ 'Alaska',
+  /* 02 */ 'Alberta',
+  /* 03 */ 'Central America',
+  /* 04 */ 'Eastern United States',
+  /* 05 */ 'Greenland',
+  /* 06 */ 'Northwest Territory',
+  /* 07 */ 'Ontario',
+  /* 08 */ 'Quebec',
+  /* 09 */ 'Western United States',
+
+  // South America
+  //
+  /* 10 */ 'Argentina',
+  /* 11 */ 'Brazil',
+  /* 12 */ 'Peru',
+  /* 13 */ 'Venezuela',
+
+  // Europe
+  //
+  /* 14 */ 'Great Britain',
+  /* 15 */ 'Iceland',
+  /* 16 */ 'Northern Europe',
+  /* 17 */ 'Scandinavia',
+  /* 18 */ 'Southern Europe',
+  /* 19 */ 'Ukraine',
+  /* 20 */ 'Western Europe',
+
+  // Africa
+  //
+  /* 21 */ 'Congo',
+  /* 22 */ 'East Africa',
+  /* 23 */ 'Egypt',
+  /* 24 */ 'Madagascar',
+  /* 25 */ 'North Africa',
+  /* 26 */ 'South Africa',
+
+  // Asia
+  //
+  /* 27 */ 'Afghanistan',
+  /* 28 */ 'China',
+  /* 29 */ 'India',
+  /* 30 */ 'Irkutsk',
+  /* 31 */ 'Japan',
+  /* 32 */ 'Kamchatka',
+  /* 33 */ 'Middle East',
+  /* 34 */ 'Mongolia',
+  /* 35 */ 'Siam',
+  /* 36 */ 'Siberia',
+  /* 37 */ 'Ural',
+  /* 38 */ 'Yakutsk',
+
+  // Australia
+  //
+  /* 39 */ 'Eastern Australia',
+  /* 40 */ 'Indonesia',
+  /* 41 */ 'New Guinea',
+  /* 42 */ 'Western Australia'
+
+];
