d46f618a20cd3d56a62c15e301d6c897ad43f04f
eudoxyz <eudoxyz@localhost.localdomain>
2017-09-20 01:09:15 +0200

    Indeksi ipak pocinju od nule (:

--- data.js
+++ data.js
@@ -2,77 +2,77 @@ const territories = [
 
   // North America
   //
-  /* 01 */ 'Alaska',
-  /* 02 */ 'Alberta',
-  /* 03 */ 'Central America',
-  /* 04 */ 'Eastern United States',
-  /* 05 */ 'Greenland',
-  /* 06 */ 'Northwest Territory',
-  /* 07 */ 'Ontario',
-  /* 08 */ 'Quebec',
-  /* 09 */ 'Western United States',
+  /* 00 */ 'Alaska',
+  /* 01 */ 'Alberta',
+  /* 02 */ 'Central America',
+  /* 03 */ 'Eastern United States',
+  /* 04 */ 'Greenland',
+  /* 05 */ 'Northwest Territory',
+  /* 06 */ 'Ontario',
+  /* 07 */ 'Quebec',
+  /* 08 */ 'Western United States',
 
   // South America
   //
-  /* 10 */ 'Argentina',
-  /* 11 */ 'Brazil',
-  /* 12 */ 'Peru',
-  /* 13 */ 'Venezuela',
+  /* 09 */ 'Argentina',
+  /* 10 */ 'Brazil',
+  /* 11 */ 'Peru',
+  /* 12 */ 'Venezuela',
 
   // Europe
   //
-  /* 14 */ 'Great Britain',
-  /* 15 */ 'Iceland',
-  /* 16 */ 'Northern Europe',
-  /* 17 */ 'Scandinavia',
-  /* 18 */ 'Southern Europe',
-  /* 19 */ 'Ukraine',
-  /* 20 */ 'Western Europe',
+  /* 13 */ 'Great Britain',
+  /* 14 */ 'Iceland',
+  /* 15 */ 'Northern Europe',
+  /* 16 */ 'Scandinavia',
+  /* 17 */ 'Southern Europe',
+  /* 18 */ 'Ukraine',
+  /* 19 */ 'Western Europe',
 
   // Africa
   //
-  /* 21 */ 'Congo',
-  /* 22 */ 'East Africa',
-  /* 23 */ 'Egypt',
-  /* 24 */ 'Madagascar',
-  /* 25 */ 'North Africa',
-  /* 26 */ 'South Africa',
+  /* 20 */ 'Congo',
+  /* 21 */ 'East Africa',
+  /* 22 */ 'Egypt',
+  /* 23 */ 'Madagascar',
+  /* 24 */ 'North Africa',
+  /* 25 */ 'South Africa',
 
   // Asia
   //
-  /* 27 */ 'Afghanistan',
-  /* 28 */ 'China',
-  /* 29 */ 'India',
-  /* 30 */ 'Irkutsk',
-  /* 31 */ 'Japan',
-  /* 32 */ 'Kamchatka',
-  /* 33 */ 'Middle East',
-  /* 34 */ 'Mongolia',
-  /* 35 */ 'Siam',
-  /* 36 */ 'Siberia',
-  /* 37 */ 'Ural',
-  /* 38 */ 'Yakutsk',
+  /* 26 */ 'Afghanistan',
+  /* 27 */ 'China',
+  /* 28 */ 'India',
+  /* 29 */ 'Irkutsk',
+  /* 30 */ 'Japan',
+  /* 31 */ 'Kamchatka',
+  /* 32 */ 'Middle East',
+  /* 33 */ 'Mongolia',
+  /* 34 */ 'Siam',
+  /* 35 */ 'Siberia',
+  /* 36 */ 'Ural',
+  /* 37 */ 'Yakutsk',
 
   // Australia
   //
-  /* 39 */ 'Eastern Australia',
-  /* 40 */ 'Indonesia',
-  /* 41 */ 'New Guinea',
-  /* 42 */ 'Western Australia'
+  /* 38 */ 'Eastern Australia',
+  /* 39 */ 'Indonesia',
+  /* 40 */ 'New Guinea',
+  /* 41 */ 'Western Australia'
 
 ];
 
 
 const graf = [
 
-  /* 01 */ [2, 6, 32],
-  /* 02 */ [1, 6, 7, 9],
-  /* 03 */ [4, 9, 13],
-  /* 04 */ [3, 7, 8, 9],
-  /* 05 */ [6, 7, 8, 15],
-  /* 06 */ [1, 2, 5, 7],
-  /* 07 */ [2, 4, 5, 6, 8, 9],
-  /* 08 */ [4, 5, 7],
-  /* 09 */ [2, 3, 4, 7],
+  /* 00 */ [1, 5, 31],
+  /* 01 */ [0, 5, 6, 8],
+  /* 02 */ [3, 8, 12],
+  /* 03 */ [2, 6, 7, 8],
+  /* 04 */ [5, 6, 7, 14],
+  /* 05 */ [0, 1, 4, 6],
+  /* 06 */ [1, 3, 4, 5, 7, 8],
+  /* 07 */ [3, 4, 6],
+  /* 08 */ [1, 2, 3, 6],
 
-]
+];
