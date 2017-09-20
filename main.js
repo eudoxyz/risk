8c0c1a1ce82b4dcace7e9a63f7ba96ce02135ddf
eudoxyz <eudoxyz@localhost.localdomain>
2017-09-20 18:04:56 +0200

    Dodaj komentare

--- main.js
+++ main.js
@@ -33,24 +33,27 @@ let mainState = {
 
     this.graphics.lineStyle(2, 0xF3F3F3, 1);
 
+    // posmatramo pravougli trougao u kojem centar prvog i centar drugog cvora obrazuju hipotenuzu
+    // interesuje nas ugao nad x-osom koji zaklapa hipotenuza
     const ADJ = dst[0] - src[0]; // nalegla strana
     const OPP = -(dst[1] - src[1]); // suprotna strana
-    let angle = Math.atan(Math.abs(OPP) / Math.abs(ADJ));
+    let angle = Math.atan(Math.abs(OPP) / Math.abs(ADJ)); // ugao
 
     if (ADJ >= 0 && OPP >= 0) ; // prvi kvadrant
     else if (ADJ < 0 && OPP >= 0) angle = Math.PI - angle; // drugi kvadrant
     else if (ADJ < 0 && OPP < 0) angle = Math.PI + angle; // treci kvadrant
     else angle = 2 * Math.PI - angle; // cetvrti kvadrant
 
+    // offsetujemo polaznu i krajnju tacku da bi grana pocinjala i zavrsavala se na ivicama cvorova (a ne u sredinama)
     const SRC_X = src[0] + Math.cos(angle) * (VERTICE_DIAMETER / 2);
     const SRC_Y = src[1] - Math.sin(angle) * (VERTICE_DIAMETER / 2);
-
     const DST_X = dst[0] + Math.cos(Math.PI + angle) * (VERTICE_DIAMETER / 2);
     const DST_Y = dst[1] - Math.sin(Math.PI + angle) * (VERTICE_DIAMETER / 2);
 
+    // izuzetak: grana od aljaske do kamcatke (luk)
     if (pair[0] === 0 && pair[1] === 31) {
 
-      const CURVATURE = 650;
+      const CURVATURE = 650; // zakrivljenost luka; veca vrednost - manja zakrivljenost
       const MIDDLE_X = (src[0] + dst[0]) / 2;
       const MIDDLE_Y = (src[1] + dst[1]) / 2;
       const DISTANCE = Math.sqrt(Math.pow(Math.abs(src[0] - dst[0]), 2) + Math.pow(Math.abs(src[1] - dst[1]), 2));
@@ -58,8 +61,12 @@ let mainState = {
       const CENTER_Y = MIDDLE_Y + Math.cos(angle) * CURVATURE;
       const RADIUS = Math.sqrt(Math.pow(CURVATURE, 2) + Math.pow(DISTANCE / 2, 2));
 
+      // 0.02 je hardcoded offset za luk da bi pocinjao i zavrsavao se na ivicama cvorova
+      // vrednost je pronadjena trial & error postupkom
+      // nema potrebe da se generalizuje jer se pojavljuje samo na ovom mestu
       this.graphics.arc(CENTER_X, CENTER_Y, RADIUS, - Math.asin(CURVATURE / RADIUS) - 0.02, Math.PI + Math.asin(CURVATURE / RADIUS) + 0.02, true, 96);
 
+    // sve ostale grane
     } else {
 
       this.graphics.moveTo(SRC_X, SRC_Y);
