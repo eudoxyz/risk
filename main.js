7869e8098ed6a718cd94d7b70be3724ab95130ae
eudoxyz <eudoxyz@localhost.localdomain>
2017-09-20 13:57:18 +0200

    Renderuj graf (prva polovina)

--- main.js
+++ main.js
@@ -1,20 +1,64 @@
-var mainState = {
+let mainState = {
 
   preload: function() {
-    game.load.image('logo', './logo.png');
+
   },
 
   create: function() {
-    this.sprite = game.add.sprite(200, 150, 'logo');
+
+    this.graphics = game.add.graphics();
+    this.graphics.lineStyle(4, 0xF3F3F3, 1);
+
+    KOORDINATE.forEach(function(k) {
+      this.graphics.drawCircle(k[0], k[1], VERTICE_DIAMETER);
+    }, this);
+
+    for (let i = 0; i < GRAF.length; i++) {
+      for (let j = 0; j < GRAF[i].length; j++) {
+        this.drawEdge(KOORDINATE[i], KOORDINATE[GRAF[i][j]]);
+      }
+    }
+
   },
 
   update: function() {
-    this.sprite.angle += 1;
+
+  },
+
+  drawEdge: function(src, dst) {
+
+    this.graphics.lineStyle(2, 0xF3F3F3, 1);
+
+    try {
+
+      const ADJ = dst[0] - src[0]; // suportna strana
+      const OPP = -(dst[1] - src[1]); // nalegla strana
+      let angle = Math.atan(Math.abs(OPP) / Math.abs(ADJ));
+
+      if (ADJ >= 0 && OPP >= 0) ; // prvi kvadrant
+      if (ADJ < 0 && OPP >= 0) angle = Math.PI - angle; // drugi kvadrant
+      if (ADJ < 0 && OPP < 0) angle = Math.PI + angle; // treci kvadrant
+      if (ADJ >= 0 && OPP < 0) angle = 2 * Math.PI - angle; // cetvrti kvadrant
+
+      const SRC_X = src[0] + Math.cos(angle) * (VERTICE_DIAMETER / 2);
+      const SRC_Y = src[1] - Math.sin(angle) * (VERTICE_DIAMETER / 2);
+
+      const DST_X = dst[0] + Math.cos(Math.PI + angle) * (VERTICE_DIAMETER / 2);
+      const DST_Y = dst[1] - Math.sin(Math.PI + angle) * (VERTICE_DIAMETER / 2);
+
+      this.graphics.moveTo(SRC_X, SRC_Y);
+      this.graphics.lineTo(DST_X, DST_Y);
+
+    } catch(err) {
+
+      ;
+
+    }
+
   }
 
 };
 
-var game = new Phaser.Game(400, 300, Phaser.AUTO, 'root');
-
+const game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'root');
 game.state.add('main', mainState);
 game.state.start('main');
