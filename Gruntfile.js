4b76aa06680e44192bb5f6da4eb090b715b1fbaf
eudoxyz <eudoxyz@localhost.localdomain>
2017-09-21 03:54:27 +0200

    Potpuna Grunt konfiguracija

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,30 +5,54 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     concat: {
-
       dist: {
-
         src: [
-
           'js/data.js',
           'js/boot.js',
           'js/load.js',
           'js/menu.js',
           'js/play.js',
           'js/game.js',
-
         ],
-
-        dest: 'js/bundle.js'
-
+        dest: 'js/bundle_es6.js'
       }
+    },
 
+    babel: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'js/bundle.js': 'js/bundle_es6.js'
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/bundle.js',
+        dest: 'js/bundle.min.js'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['default'],
+        options: {
+          spawn: false
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat', 'babel', 'uglify']);
 
 }
--- js/bundle_es6.js
+++ js/bundle_es6.js
@@ -195,3 +195,172 @@ const KOORDINATE = [
   /* 41 */ [768, 521]
 
 ]
+
+const bootState = {
+
+  create: function() {
+
+    game.canvas.oncontextmenu = e => e.preventDefault();
+    game.stage.backgroundColor = "#1291ee";
+    game.stage.disableVisibilityChange = true;
+
+    game.state.start('load');
+
+  }
+
+}
+
+const loadState = {
+
+  preload: function() {
+
+    game.load.audio('nightRain', ['assets/audio/airtone_-_nightRain.ogg']);
+    game.load.audio('tick', ['assets/audio/tick.mp3']);
+
+  },
+
+  create: function() {
+
+    game.state.start('menu');
+
+  }
+
+}
+
+const menuState = {
+
+  create: function() {
+
+    const message = game.add.text(
+      game.world.centerX,
+      game.world.centerY,
+      'Klikni mišem za početak',
+      { font: '48px Arial', fill: '#F3F3F3' }
+    );
+    message.anchor.setTo(.5, .5);
+
+    const click = game.input.mousePointer.leftButton;
+    click.onDown.addOnce(function() {
+      game.state.start('play');
+    });
+
+  }
+
+}
+
+const playState = {
+
+  create: function() {
+
+    game.add.audio('nightRain').play();
+    this.tickSound = game.add.audio('tick');
+
+    this.drawEdges();
+    this.drawVertices();
+
+  },
+
+  update: function() {
+
+    //
+
+  },
+
+  // ================ //
+  // HELPER FUNCTIONS //
+  // ================ //
+
+  drawEdge: function(src, dst, pair) {
+    const grane = game.add.graphics();
+    grane.lineStyle(2, 0xF3F3F3, 1);
+
+    // grana od aljaske do kamcatke
+    if (pair[0] === 0 && pair[1] === 31) {
+
+      const CURVATURE = 650;
+      const MIDDLE_X = (src[0] + dst[0]) / 2;
+      const MIDDLE_Y = src[1]; // y-koordinata im je ista
+      const DISTANCE = dst[0] - src[0];
+      const RADIUS = Math.sqrt(Math.pow(CURVATURE, 2) + Math.pow(DISTANCE / 2, 2));
+
+      grane.arc(MIDDLE_X, MIDDLE_Y + CURVATURE, RADIUS, - Math.asin(CURVATURE / RADIUS), Math.PI + Math.asin(CURVATURE / RADIUS), true, 96);
+
+    // sve ostale grane
+    } else {
+
+      grane.moveTo(src[0], src[1]);
+      grane.lineTo(dst[0], dst[1]);
+
+    }
+  },
+
+
+  drawEdges: function() {
+    const drawn = [...Array(42).keys()].map(i => Array(42)); // 42 x 42 niz
+    for (let i = 0; i < GRAF.length; i++) {
+      for (let j = 0; j < GRAF[i].length; j++) {
+        if (!drawn[i][GRAF[i][j]]) {
+          this.drawEdge(KOORDINATE[i], KOORDINATE[GRAF[i][j]], [i, GRAF[i][j]]);
+          drawn[i][GRAF[i][j]] = drawn[GRAF[i][j]][i] = true;
+        }
+      }
+    }
+  },
+
+
+  drawVertices: function() {
+    const cvorovi = game.add.group();
+    cvorovi.inputEnableChildren = true;
+
+    for (let i = 0; i < KOORDINATE.length; i++) {
+
+      const cvor = game.add.graphics();
+      cvor.lineStyle(4, 0xF3F3F3, 1);
+
+      let color;
+      if (i < 9){
+        color = 0xFF0000;
+      } else if (i < 13) {
+        color = 0x006600;
+      } else if (i < 20) {
+        color = 0x0033cc;
+      } else if (i < 26) {
+        color = 0x000000;
+      } else if (i < 38) {
+        color = 0xffcc00;
+      } else {
+        color = 0xcc00cc;
+      }
+      cvor.beginFill(color, 1);
+
+      cvor.drawCircle(KOORDINATE[i][0], KOORDINATE[i][1], VERTICE_DIAMETER);
+      cvor.endFill();
+      cvor.data.tenkici = game.add.text(KOORDINATE[i][0], KOORDINATE[i][1], '0', { font: '16px Arial', fill: '#ffffff' });
+      cvor.data.tenkici.anchor.setTo(0.5, 0.4);
+      cvor.events.onInputDown.add(function(cvor, pointer) {
+        if (pointer.leftButton.isDown) {
+          cvor.data.tenkici.text = String(++game.global.tenkici[cvorovi.getChildIndex(cvor)]);
+          this.tickSound.play();
+        }
+      }, this);
+      cvorovi.add(cvor);
+    }
+
+  },
+
+};
+
+const game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'root');
+
+game.global = {
+
+  tenkici: new Array(42).fill(0)
+
+};
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+
+game.state.start('boot');
