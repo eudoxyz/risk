a3f870e628a4111833ed7846034bd96efb712a24
F34n0r <zivko.raicevic@gmail.com>
2017-09-21 04:41:02 +0200

    Dugme za gašenje zvuka

--- js/play.js
+++ js/play.js
@@ -4,10 +4,12 @@ const playState = {
 
     game.add.audio('nightRain').play();
     this.tickSound = game.add.audio('tick');
-
+    var s = game.add.sprite(80, 0, 'sound');
+    
+    
     this.drawEdges();
     this.drawVertices();
-
+    var s = game.add.sprite(80, 0, 'sound');
   },
 
   update: function() {
